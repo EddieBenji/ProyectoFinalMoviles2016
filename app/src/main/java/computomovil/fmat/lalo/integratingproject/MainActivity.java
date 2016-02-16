d60324a7ff0f7bcec72a6aaf1ec6d18c20ea660a
Lalo_Canche <eduardo_benji@hotmail.com>
2016-02-16 13:36:35 -0600

    Initial commit

---
+++ app/src/main/java/computomovil/fmat/lalo/integratingproject/MainActivity.java
@@ -0,0 +1,13 @@
+package computomovil.fmat.lalo.integratingproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
